77a51fb8133ca8d22721ce778ea7de9ebdeb92c0
Gonzalo Velasco <vela8624@gmail.com>
2023-07-28 14:29:04 -0300

    init project

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+
+// src/app.ts
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express with TypeScript!');
+});
+
+export default app;
