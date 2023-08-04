b6b05590bbaf65a47901fa473a9c13dfe9238156
Gonzalo Velasco <vela8624@gmail.com>
2023-08-04 17:30:42 -0300

    add nft validator

---
+++ src/api/nft/nftValidator.ts
@@ -0,0 +1,22 @@
+import Joi from 'joi';
+
+export const nftDataSchema = Joi.object({
+  collectionAddress: Joi.string().required(),
+  ownerAddress: Joi.string().required(),
+  tokenId: Joi.number().integer().required(),
+  name: Joi.string().required(),
+  type: Joi.string().valid('fixed', 'auction').required(),
+  price: Joi.when('type', {
+    is: 'fixed',
+    then: Joi.number().required(),
+    otherwise: Joi.number(),
+  }),
+  auction: Joi.when('type', {
+    is: 'auction',
+    then: Joi.object({
+      minimumBid: Joi.number().required(),
+      endDate: Joi.string().isoDate().required(),
+    }).required(),
+    otherwise: Joi.forbidden(),
+  }),
+});
