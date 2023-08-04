bdb684657d33b38ffe86e110d526c7d358516074
Gonzalo Velasco <vela8624@gmail.com>
2023-08-04 17:31:34 -0300

    add routes to the app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
-
-// src/app.ts
 import express from 'express';
+import dotenv from 'dotenv-safe';
+import bodyParser from 'body-parser';
+import nftRoutes from './api/nft/nftRoutes';
+
+dotenv.config();
 
 const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express with TypeScript!');
+app.use(bodyParser.json());
+
+app.use('/api', nftRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 export default app;
